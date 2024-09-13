c8261edf1e8b355292ec195c03c6ff73162e69b8
rkumar076 <rahul76.mnnit@gmail.com>
2024-09-13 14:08:26 +0000

    data type

---
+++ jscac/lec5.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as newer version
+
+
+ // alert(3+3) // we are using nodejs  diectly use karne se error aayega browsser me nahi aata error  resone aage padenge 
+ console.log(3+3)
+ console.log("rahul")
+
+ // data type
+ // number
+ // bigint
+ //string =>""
+ //null=>standalone value
+ //undefined=>
+// symbol=>unique
+
+ console.log(typeof undefined); //undefined
+ console.log(typeof null); // object
+
+
+ 
+
