7c965a09f465d2e5832d2c10ade2ac6a84178d1e
rkumar076 <rahul76.mnnit@gmail.com>
2024-09-13 14:27:00 +0000

    change into

---
+++ jscac/lec6.js
@@ -0,0 +1,22 @@
+let score=33
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33"=>33  string to number me change karne pe 
+//"33abc"=>NaN  isme not a number show ho rah hain due  abc ko change nahi klar arha hain number me
+// true=>1; false=>0;
+
+ let a=1;
+ let booleana=Boolean(a);
+ console.log(booleana);
+
+ let someNumber=33
+ let stringNumber=String(someNumber);
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
+ 
