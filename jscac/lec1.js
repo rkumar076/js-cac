1210d0bb8fb92cf5fc0a5f096994f1f7554ad03e
rkumar076 <rahul76.mnnit@gmail.com>
2024-09-13 13:50:47 +0000

    var const lec completed

--- jscac/lec1.js
+++ jscac/lec1.js
@@ -1,1 +1,16 @@
 console.log("aur bato kya hal chal hain")
+//const  variable can fix the value of data into the entire from that  you will write
+  const accountId=12422
+
+
+  let accountEmail="rahul76.mnnit.com"
+   var shivam="rahul"
+//   Variables declared by let are only available inside the block where they're defined. 
+//   Variables declared 
+//   by var are available throughout the function in which they're declared.
+    // 
+
+
+    console.log(accountId);
+    console.table([accountId,accountEmail]);
+
