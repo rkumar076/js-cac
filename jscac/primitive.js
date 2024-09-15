57ee4ffefec433ba754a6562b55f91867bbae444
rkumar076 <rahul76.mnnit@gmail.com>
2024-09-15 07:09:48 +0000

    primitive  hain

---
+++ jscac/primitive.js
@@ -0,0 +1,25 @@
+//primitive
+   // 7 type
+     // string ,number ,boolean ,null,undefined,symbol,BigInt
+const id=Symbol('123')
+const anotherId=Symbol('123')
+console.log(id==anotherId);
+const bigNUmber=123124312323342n
+
+
+
+
+
+
+
+//Reference(non primitive)
+ // array ,objects,function 
+ 
+ const heros=["shaktiman ","naagraj","doga"];
+ let myObj={
+    name:"hitesh",
+    age:22,
+ }
+
+
+// detail me padh lena kya ki kon sa data type kya output deti hain interview ke liye important hain
