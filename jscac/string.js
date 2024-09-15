e484f50ab5ec7338fe5d061f4a7b506cff761097
rkumar076 <rahul76.mnnit@gmail.com>
2024-09-15 18:53:37 +0000

    string

---
+++ jscac/string.js
@@ -0,0 +1,32 @@
+const name ="hitesh"
+const repoCount=50
+
+
+// is syntak me use karne se readblity aaasan hoti hain  and ye nest hain ieme varirable ke ender v aap operation kar payegeg to readlblity  easy hogy 
+// console.log(`hello bhai mera name hain  ${name} and my repo count is ${repoCount}`);
+ const gameName= new String('rahulkumar'); //  creating an object of an string 
+
+  console.log(gameName);  // printing a  string 
+
+   console.log(gameName.length);    // lenght method for finding the length of string 
+   console.log(gameName.charAt(2)); //  charecter batayega us index ka 
+
+
+   const newString=gameName.substring(0,4);
+   console.log(newString);
+
+
+   const  anotherString=gameName.slice(-8,5); // can be used to extract a portion of an array or string, or to create a new array or string based on an existing one:
+   console.log(anotherString);
+ // front aur back se hi trim karega
+    const newStringOne="      rahul  kumar.    "; // ye use ho sakta hain jab aap form detail le  raho ho  aut koi aapna name bharte time usme spaces bewajh bhar diya 
+     console.log(newStringOne);
+     console.log(newStringOne.trim());  // ye use ho sakta hain jab aap form detail le  raho ho  aut koi aapna name bharte time usme spaces bewajh bhar diya  
+  
+
+    //  example of  replaces
+
+    const url=
+
+
+
