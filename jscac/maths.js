285a66d1422f43ac28a69683b16f48ca2502c6bc
rkumar076 <rahul76.mnnit@gmail.com>
2024-09-15 19:34:15 +0000

    maths

---
+++ jscac/maths.js
@@ -0,0 +1,41 @@
+// const score=400;
+// const number= new Number(1000) //  number v ek object   hain
+// console.log(number.toString());
+// console.log(number.toString().length);
+//   // properties of the number 
+  
+//   const otherNumber=23.4234;
+
+//   console.log(otherNumber);
+
+  //+++++++++++++ Maths ++++++++++++++
+
+
+  console.log(Math); // ye v ek object hain  jisme abhut sare properties hain
+
+  console.log(Math.abs(-3));
+   console.log(round(4.6));  //o/p=5
+   console.log(round(4.4));  //o/p=4
+  console.log(Math.ceil(4.2));  // o/p=5
+  console.log(Math.floor(4.9)); // o/p=4
+  console.log(Math.min(4,3,2,34)); // its give the minimum nunber of the given number 
+  console.log(Math.max(2,42,2,52,1)); // its give you a maximum value of the given number
+
+
+  //+++++++Math.random+++++++
+
+   console.log(Math.random()); // ye always give you  the number between 0 to 1
+
+
+    // use kar sakte  hain dice banane me  like 
+
+
+     console.log(Math.floor(Math.random()*6+1));
+ // impottant concept   hain
+ 
+   const min=10
+    const max=20
+
+     console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
---
+++ jscac/numberAndMaths.js
@@ -0,0 +1,16 @@
+// const score=400;
+// const number= new Number(1000) //  number v ek object   hain
+// console.log(number.toString());
+// console.log(number.toString().length);
+//   // properties of the number 
+  
+//   const otherNumber=23.4234;
+
+//   console.log(otherNumber);
+
+  //+++++++++++++ Maths ++++++++++++++
+
+
+  console.log(Math); // ye v ek object hain  jisme abhut sare properties hain
+
+  console.log(Math.abs(-3));
